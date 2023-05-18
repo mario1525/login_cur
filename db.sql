2d0d515ae28e35c78cd9289faa286c17d3697466
mario1525 <mbeltran4892@gmail.com>
2023-05-18 16:26:27 -0500

    add users

--- db.sql
+++ db.sql
@@ -6,4 +6,12 @@ CREATE TABLE  usuario (
  	PRIMARY KEY (id)
 )
 
-INSERT INTO usuario VALUES ("admin admin ", "admin@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("valentina espinosa ", "valentina.espinosa@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("robert machacon ", "robert.machacon@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("orlay Marchena ", "orlay.marchena@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("Mario beltran ", "mario.beltran@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("alexander Rodrigues ", "alexander.Rodrigues@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("ines rodrigues ", "ines.rodrigues@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("madhelein perez ", "madhelein.perez@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("sebastian perez ", "sebastian.perez@unireformada.edu.co", "admin345");
+INSERT INTO usuario (nombre, email, passw) VALUES ("heider gonzales", "heider.gonzales@unireformada.edu.co", "admin345");
